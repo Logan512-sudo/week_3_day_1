3cff61ead954b633b9d5b4e13ec0b07edc0912dc
Lara <lmlara1@cps.k12.il.us>
2024-09-12 09:14:04 -0500

    Week 3: Day 2

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -102,19 +102,6 @@ print(f"The date of birth is {date_of_birth} and the number is {number2} and the
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 ############################################################################################
 
 
@@ -133,47 +120,64 @@ print(f"The date of birth is {date_of_birth} and the number is {number2} and the
 
 ##########################################################################################
 # #addition
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(6/2)
 # #modulo
+print(7%4) #remainder of 7 divided by 4
 # #powers
+print(2**3) # 2 to the power of 3
 # #get the max and min of a number
+print("the max of 2 and 3 is",max(2,3)) #biggest number
+print("the min of 2 and 3 is",min(2,3)) #lowest number
 # #round a number
+print("round 3.9 is",round(3.9))
 # # absolute value
+print("the absolute value of -3 is",abs(-3))
 # # order of operations
+print("2 + 10 * 10 + 3",2 + 10 * 10 +3)
+# order of operations is the same as in math
+# it means do the multiplication first
+# then do the addition
+
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *   
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print("the floor of 3.7 is",floor(3.7))
+print("the floor of 5.8 is",floor(5.8))
 # #ceil method
+print("the ceil of 3.7 is",ceil(3.7))
+print("the ceil of 5.8 is",ceil(5.8))
 # #sqrt method
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 ##########################################################################################
-# So what have we learned? We learned some of the basics of numbers in Python. We also learned how to do arithmetic and use Python as a basic calculator. We then wrapped it up with learning about Variable Assignment in Python.
+# So what have we learned? We learned some of the basics of numbers in Python.
+# We also learned how to do arithmetic and use Python as a basic calculator. 
+# We then wrapped it up with learning about Variable Assignment in Python.
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name = input("What is your name?")
+print("hello",name)
 # # basic math calculator
 # #ask the user for 2 numbers
+num1 = int(input("enter a number:"))
+num2 = int(input("enter another number:"))
 # # print out a statement where you:
 # # add them together
+print(num1 + num2)
 # #multiply
+print(num1 * num2)
 # # find the max number
+print(max(num1,num2))
 # # find the remainder of the numbers
+print(num1%num2)
 # #round one number
+print(round(num1 + num2))
 
 
 
